73589c6756b8b22062b86c7395e8ac52de9b0fef
MinhTuan083 <yuunohop3@gmail.com>
2024-05-06 23:45:04 +0700

    update new routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Car;
+use App\Http\Controllers\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('/dashboard', [CarController::class, 'index'])->name('dashboard');
+Route::get('/addcar', [CarController::class, 'AddCar'])->name('addcar');
+Route::post('/addcar', [CarController::class, 'storeCar'])->name('cars.store');
+Route::get('/list', [CarController::class, 'listCar'])->name('list');
 Route::get('/', function () {
     return view('welcome');
 });
