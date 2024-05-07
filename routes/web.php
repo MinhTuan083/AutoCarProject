fa5fb72606c866a7cfca346e3f4d04f8eb9a7408
MinhTuan083 <yuunohop3@gmail.com>
2024-05-07 23:12:19 +0700

    add new routes [ show, delete, edit ]

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/dashboard', [CarController::class, 'index'])->name('dashboard');
 Route::get('/addcar', [CarController::class, 'AddCar'])->name('addcar');
 Route::post('/addcar', [CarController::class, 'storeCar'])->name('cars.store');
 Route::get('/list', [CarController::class, 'listCar'])->name('list');
+Route::get('/show/{id}', [CarController::class, 'showCar'])->name('cars.show');
+Route::delete('delete/{id}', [CarController::class, 'deleteCar'])->name('cars.delete');
+Route::get('edit-car/{id}', [CarController::class, 'editCar'])->name('cars.edit');
+Route::put('update-car/{id}', [CarController::class, 'updateCar'])->name('cars.update');
+
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
