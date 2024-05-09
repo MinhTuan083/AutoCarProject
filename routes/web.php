55114a0e972c10ad7aa568ff8917a0a6f8bd2331
MinhTuan083 <yuunohop3@gmail.com>
2024-05-09 17:39:43 +0700

    update new routes, edit, update, delete, show, list brand

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Car;
 use App\Http\Controllers\CarController;
+use App\Models\Brand;
+use App\Http\Controllers\BrandController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,13 +20,19 @@ use App\Http\Controllers\CarController;
 Route::get('/dashboard', [CarController::class, 'index'])->name('dashboard');
 Route::get('/addcar', [CarController::class, 'AddCar'])->name('addcar');
 Route::post('/addcar', [CarController::class, 'storeCar'])->name('cars.store');
-Route::get('/list', [CarController::class, 'listCar'])->name('list');
-Route::get('/show/{id}', [CarController::class, 'showCar'])->name('cars.show');
-Route::delete('delete/{id}', [CarController::class, 'deleteCar'])->name('cars.delete');
+Route::get('/listcar', [CarController::class, 'listCar'])->name('listcar');
+Route::get('/showcar/{id}', [CarController::class, 'showCar'])->name('cars.show');
+Route::delete('delete-car/{id}', [CarController::class, 'deleteCar'])->name('cars.delete');
 Route::get('edit-car/{id}', [CarController::class, 'editCar'])->name('cars.edit');
 Route::put('update-car/{id}', [CarController::class, 'updateCar'])->name('cars.update');
 
-
+Route::get('/addbrand', [BrandController::class, 'addBrand'])->name('addbrand');
+Route::post('/addbrand', [BrandController::class, 'storeBrand'])->name('brands.store');
+Route::get('/listbrand', [BrandController::class, 'listBrand'])->name('listbrand');
+Route::get('/showbrand/{id}', [BrandController::class, 'showBrand'])->name('brands.show');
+Route::get('edit-brand/{id}', [BrandController::class, 'editBrand'])->name('brands.edit');
+Route::post('update-brand/{id}', [BrandController::class, 'updateBrand'])->name('brands.update');
+Route::delete('delete-brand/{id}', [BrandController::class, 'deleteBrand'])->name('brands.delete');
 
 
 Route::get('/', function () {
