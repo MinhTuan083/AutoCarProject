96a255df129269e4fdd900fa9beecd3f25a5858b
phi1235 <phipari12345@gmail.com>
2024-05-16 18:49:27 +0700

    commit new employee

--- app/Http/Controllers/CRUDEmployeeController.php
+++ app/Http/Controllers/CRUDEmployeeController.php
@@ -1,10 +1,100 @@
 <?php
+    namespace App\Http\Controllers;
+    
+    use App\Models\Employee;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Hash;    
+    class CRUDEmployeeController extends Controller
+    {
+        public function listEmp()
+    {
+        $employees = Employee::orderBy('created_at', 'desc')->get();
+        return view('crud-car.listEmployee', compact('employees'));
+    }
 
-namespace App\Http\Controllers;
+    public function create()
+    {
+        return view('crud-car.addEmployee');
+    }
 
-use App\Models\Employee;
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|regex:/^[\p{L} ]+$/u',// ràng buộc trường name chỉ được nhập chữ cai và không được nhập kí tự đặc biệt hoặc số
+            'email' => 'required|email|regex:/@gmail\.com$/|unique:employees,email',// ràng buộc đuôi @gmail.com cho email của nhân viên
+            'phone' => 'numeric|string|min:10|unique:employees,phone',// người dùng nhập chũ thì không thể lưu vào db ở trường này
+            'address' => 'nullable|string',
+            'sex' => 'required|in:male,female',
+            'password' => 'required|string|min:6|confirmed',
+        ], [
+            'email.regex' => 'Địa chỉ email phải kết thúc bằng @gmail.com.',// thông báo cho trường email
+        ]);
 
-class CRUDStaffController extends Controller
-{
+        $employee = new Employee;
+        $employee->name = $request->input('name');
+        $employee->email = $request->input('email');
+        $employee->phone = $request->input('phone');
+        $employee->address = $request->input('address');
+        $employee->sex = $request->input('sex');
+        $employee->password = Hash::make($request->input('password'));
 
-}
+      
+
+        $employee->save();
+
+        return redirect()->route('viewEmp')->with('success', 'Nhân viên đã được thêm thành công!');
+    }
+    public function edit($id)
+    {
+        $employee = Employee::findOrFail($id);
+        return view('crud-car.editEmployee', compact('employee'));
+    }
+
+    public function update(Request $request, Employee $employee)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|regex:/^[\p{L} ]+$/u',
+            'email' => 'required|email|regex:/@gmail\.com$/|unique:employees,email' . $employee->id,
+            'phone' => 'required|string|min:10|unique:employees,phone',
+            'address' => 'nullable|string',
+            'sex' => 'required|in:male,female',
+            'password' => 'nullable|string|min:6|confirmed',
+        ], [
+            'email.regex' => 'Địa chỉ email phải kết thúc bằng @gmail.com.',// thông báo cho trường email
+        ]);
+
+        $employee->name = $request->input('name');
+        $employee->email = $request->input('email');
+        $employee->phone = $request->input('phone');
+        $employee->address = $request->input('address');
+        $employee->sex = $request->input('sex');
+
+        if ($request->input('password')) {
+            $employee->password = Hash::make($request->input('password'));
+        }
+
+      
+
+        $employee->save();
+        
+
+        return redirect()->route('viewEmp')->with('success', 'Thông tin nhân viên đã được cập nhật thành công!');
+    }
+    public function deleteEmp($id)
+    {
+        // Tìm và xoá loại xe dựa vào id
+        $employee = Employee::findOrFail($id);
+     
+
+        $employee->delete();
+        // Chuyển hướng về danh sách loại xe với thông báo thành công
+
+        if (!$employee) {
+            return redirect()->back()->with('error', 'Cartype not found');
+        }
+        return redirect()->route('viewEmp')->with('success', 'Nhân viên đã được xoá thành công');
+
+    }
+    }
+    
+
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\CRUDCarController;
+use App\Http\Controllers\CRUDEmployeeController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('dashboard', [CRUDCarController::class, 'index'])->name('dashboard');
+Route::get('car-types', [CRUDCarController::class, 'listCartp'])->name('view');
+Route::get('create', [CRUDCarController::class, 'create'])->name('car-types.create');
+Route::get('addcar', [CRUDCarController::class, 'create'])->name('addcar');
+Route::post('addcar', [CRUDCarController::class, 'store'])->name('cars.store');
+Route::get('edit/{id}', [CRUDCarController::class, 'edit'])->name('edit');
+Route::put('update/{carType}', [CRUDCarController::class, 'update'])->name('update');
+Route::delete('delete/{id}', [CRUDCarController::class, 'deleteCar'])->name('delete');
+
+
+Route::get('employee', [CRUDEmployeeController::class, 'listEmp'])->name('viewEmp');
+Route::get('addeployee', [CRUDEmployeeController::class, 'create'])->name('employees.create');
+Route::post('addeployee', [CRUDEmployeeController::class, 'store'])->name('employees.store');
+Route::get('employees/{id}/edit', [CRUDEmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('employees/{employee}', [CRUDEmployeeController::class, 'update'])->name('employees.update');
+Route::delete('delete/{id}', [CRUDEmployeeController::class, 'deleteEmp'])->name('deleteEmp');
